832e6e68c2c9d3be12af4de48c55fa5003f9194a
Angel Perez <fibble@angels-mbp-2.lan>
2021-12-19 22:35:34 -0500

    feat: add infinite scroll js functions

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import "./style.css";
 
-document.addEventListener("DOMContentLoaded", function () {
-	console.log("DOM Ready!");
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+document.addEventListener("scroll", () => {
+	lastKnownScrollPosition = window.scrollY;
+	if (
+		document.getElementsByClassName("app").length > 1 &&
+		lastKnownScrollPosition === 0
+	) {
+		removeNode(1);
+	}
+	if (!ticking) {
+		window.requestAnimationFrame(() => {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+				cloneNode();
+			}
+			ticking = false;
+		});
+		ticking = true;
+	}
 });
+
+function cloneNode() {
+	const app = document.getElementsByClassName("app")[0].cloneNode(true);
+	document.getElementsByTagName("main")[0].append(app);
+	if (document.getElementsByClassName("app").length > 2) removeNode(0);
+}
+
+function removeNode(index) {
+	document.getElementsByClassName("app")[index].remove();
+}
