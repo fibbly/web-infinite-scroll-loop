26839a274d4dc8889154dc7a849726b760a7686a
Angel Perez <fibble@angels-mbp-2.lan>
2021-12-23 18:02:54 -0500

    feat: add infinite scroll up

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 import "./style.css";
 
 let lastKnownScrollPosition = 0;
-let ticking = false;
+
+// this is the element that will be duplicated when user scrolls up or down
+const node = document.getElementsByClassName("app")[0];
+
+// this is the parent element that will contain all of the duplicated elements
+const parent = document.getElementsByTagName("main")[0];
 
 document.addEventListener("scroll", () => {
+	/*
+	 * The read-only scrollY property of the Window interface returns
+	 * the number of pixels that the document is currently scrolled vertically.
+	 */
 	lastKnownScrollPosition = window.scrollY;
-	if (
-		document.getElementsByClassName("app").length > 1 &&
-		lastKnownScrollPosition === 0
-	) {
-		removeNode(1);
-	}
-	if (!ticking) {
-		window.requestAnimationFrame(() => {
-			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-				cloneNode();
-			}
-			ticking = false;
-		});
-		ticking = true;
-	}
+
+	window.requestAnimationFrame(() => {
+		// detect if user has scrolled to the bottom of the page
+		if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+			appendNode();
+		}
+		// detect if user has scrolled to the top of the page
+		if (lastKnownScrollPosition < 1) {
+			prependNode();
+		}
+	});
 });
 
-function cloneNode() {
-	const app = document.getElementsByClassName("app")[0].cloneNode(true);
-	document.getElementsByTagName("main")[0].append(app);
-	if (document.getElementsByClassName("app").length > 2) removeNode(0);
+function prependNode() {
+	console.log("top");
+	const firstNode = document.getElementsByClassName("app")[0];
+	parent.prepend(node.cloneNode(true));
+	window.scrollTo(0, firstNode.offsetTop);
+	// remove the last node in the list of duplicate elements
+	removeNode(document.getElementsByClassName("app").length - 1);
+}
+
+function appendNode() {
+	console.log("bottom");
+	parent.append(node.cloneNode(true));
+	// remove a node if duplicate elements exceed more than two
+	if (document.getElementsByClassName("app").length > 2) {
+		removeNode(0);
+	}
 }
 
 function removeNode(index) {
